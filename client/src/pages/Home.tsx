11dcf43f51fa6661f5fdef3bc0f1fa34d6e70da3
Karan <karanpargal007@gmail.com>
2023-12-09 02:18:52 +0530

    Push wallet connect

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,14 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 export default function Home() {
   return (
     <main className="min-h-screen">
       <div className="container mx-auto my-16">
         <h1 className="text-7xl">HelpFyndr</h1>
         <h2 className="">Insert a catchy line.</h2>
+      </div>
+      <div>
+        <ConnectButton />
       </div>
     </main>
   );
