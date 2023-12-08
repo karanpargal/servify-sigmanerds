637b1f47947dc35859fdd22070feae37a648be01
Yashaswini Singh Shaktawat <yashaswinisinghshaktawat@gmail.com>
2023-12-09 01:25:26 +0530

    Added Consumer Dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,12 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Auth from '@/pages/Auth';
 import Home from '@/pages/Home';
 
+import ConsumerDashboard from '@/pages/ConsumerDashboard';
+
 const router = createBrowserRouter([
   { path: '/', Component: Home },
   { path: '/auth', Component: Auth },
+  { path: '/consumer/dashboard', Component: ConsumerDashboard},
 ]);
 
 function App() {
