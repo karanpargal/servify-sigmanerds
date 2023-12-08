6a5a2c9b4c20e804e35c53bbd5f1a1152c05f647
Snehil <51155558+SneakySensei@users.noreply.github.com>
2023-12-08 22:56:36 +0530

    init react, tailwind, react router, framer motion, hero icons

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Auth from './pages/Auth';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  { path: '/', Component: Home },
+  { path: '/auth', Component: Auth },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ client/src/components/shared/HeroIcons.tsx
@@ -0,0 +1,3 @@
+import * as HeroIcons from '@heroicons/react/24/outline';
+
+export default HeroIcons;
