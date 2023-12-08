d5b5dad279330326fbbc77d2445d38315f5e332d
Karan <karanpargal007@gmail.com>
2023-12-08 21:20:10 +0530

    Add backend boilerplate

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+import express, { type Express, type Request, type Response } from "express";
+import { config as DotenvConfig } from "dotenv";
+import { connectDB } from "./utils/connectDB";
+import cors from "cors";
+DotenvConfig();
+connectDB();
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+app.use("*", (req: Request, res: Response) => {
+  res.status(404).send("Not Found");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
