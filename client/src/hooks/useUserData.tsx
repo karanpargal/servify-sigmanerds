916859a6f7b0b1609af1b84e214a0f117c49ec4c
Karan <karanpargal007@gmail.com>
2023-12-09 14:14:37 +0530

    get user details

--- client/src/hooks/useUserData.tsx
+++ client/src/hooks/useUserData.tsx
@@ -1,6 +1,36 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import useWallet from './useWallet';
+
 /**
- * @returns off chain and on chain data for the current user
+ * @returns an object containing data, loading, and error for the current user
  */
 export default function useUserData() {
-  return null;
+  const { address } = useWallet();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `http://localhost:8080/api/v1/users/${address}`,
+        );
+        setData(response.data);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (address) {
+      fetchData();
+    }
+  }, [address]);
+
+  return { data, loading, error };
 }
